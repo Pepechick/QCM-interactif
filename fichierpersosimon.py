48e74f03cfc8dbea3fcae942859247afaa31b0a6
Simon <Simonloison2@gmail.com>
2025-10-06 12:15:24 +0200

    j'ai tout mis dans la classe sauf qu'il manque qulques trucs

--- fichierpersosimon.py
+++ fichierpersosimon.py
@@ -1,41 +1,43 @@
 from dictionnaires import DictioQCM
 
-def affiche_correction(question):
-        """ fonction qui affiche la correction à la question i
+class questionnairetressimple:
+
+    def __init__(self, QCM): 
+        self.QCM = QCM
+
+    def affiche_correction(self, question):
+        """ fonction qui affiche la correction à la question
         --> renvoie un tuple avec (indice de la réponse , réponse correcte)"""
         
-        for cle, valeur in DictioQCM.items():
+        for cle, valeur in self.QCM.items():
             if cle == question:
                 for i in range(len(valeur)):
                     if valeur[i][-1] == "*":
-                        reponse = valeur[i][:-1]  # on enlève l’étoile
-                        return (i+1, reponse)
+                        self.reponse = valeur[i][:-1]  # on enlève l’étoile
+                        return (i+1, self.reponse)
 
         return None
 
-def affiche_gag(question):
-        """ fonction qui affiche la correction à la question i
-        --> renvoie un tuple avec (indice de la réponse , réponse correcte)"""
+    def affiche_gag(self, question):
+        """ fonction qui affiche la réponse drôle
+        --> renvoie un tuple avec (indice de la réponse , réponse spéciale drôle)"""
         
-        for cle, valeur in DictioQCM.items():
+        for cle, valeur in self.QCM.items():
             if cle == question:
                 for i in range(len(valeur)):
                     if valeur[i][-1] == "!":
-                        reponse = valeur[i][:-1]  # on enlève l’étoile
-                        return (i+1, reponse)
+                        self.reponse = valeur[i][:-1]  # on enlève le point d'!
+                        return (i+1, self.reponse)
 
         return None
 
-print(affiche_gag("drapeaux/France.png"))
-print(affiche_correction("drapeaux/France.png"))
-
-def affichageQCM(question):
-        ''' permet d'afficher la question n°i du QCM contenue dans le tableau QCM.
+    def affichageQCM(self, question):
+        ''' permet d'afficher la question du QCM contenue dans le tableau QCM.
         les réponses sont ensuite proposées (l'étoile indiquant la bonne réponse est supprimée lors de l'affichage)'''
         
         print(f"Question : {question}")
 
-        for cle, valeur in DictioQCM.items():
+        for cle, valeur in self.QCM.items():
             if cle == question:
                 for i in range(len(valeur)):
                     if valeur[i][-1] == "*":
@@ -45,80 +47,23 @@ def affichageQCM(question):
                     else : 
                         print(i+1, " : ", valeur[i])
 
-        r = int(input("Entrez le N° de votre réponse (1, 2, ou 3) : "))
+        self.r = int(input("Entrez le N° de votre réponse (1, 2, ou 3) : "))
 
-        return r
+        return self.r
+        
 
-def score_reponse(rep, question):
+    def score_reponse(self, rep, question):
         ''' calcul le score de la question n°i en fonction de la réponse
         --> renvoie 0 ou 1'''
         
-        for cle, valeur in DictioQCM.items():
+        for cle, valeur in self.QCM.items():
             if cle == question:
                 for i in range(len(valeur)):
                     if valeur[i][-1] == "*":
-                        rep_correcte = valeur[i][:-1]
+                        self.rep_correcte = valeur[i][:-1]
     
-        if rep_correcte == rep:
-            return 1
-        else:
-            return 0
-
-print(score_reponse(affichageQCM("drapeaux/France.png"), "drapeaux/France.png"))
-
-
-class questionnairetressimple:
-
-    def __init__(self): 
-        self.QCM = DictioQCM
-
-
-    def affiche_correction(self, i):
-        ''' fonction qui affiche la correction à la question i
-        --> renvoie un tuple avec (indice de la réponse , réponse correcte)'''
-        
-        assert type(self.QCM) != 'list', "QCM doit être un tableau."
-        assert i >= 0 and i <= 2, "i est compris entre 0 et 2."
-        
-        for k in range(1, 4):
-            if self.QCM[k][i][0] == "*":
-                reponse = self.QCM[k][i][1:]
-                correction = (i, reponse)
-
-        return correction
-
-
-    def affichageQCM(self, i):
-        ''' permet d'afficher la question n°i du QCM contenue dans le tableau QCM.
-        les réponses sont ensuite proposées (l'étoile indiquant la bonne réponse est supprimée lors de l'affichage)'''
-        
-        assert type(self.QCM) != 'list', "QCM doit être un tableau."
-        assert i >= 0 and i <= 2, "i est compris entre 0 et 2."
-        
-        print(self.QCM[0][i])
-        
-        for k in range(1, 4):
-            if self.QCM[k][i][0] == "*":
-                print(k, " : ", self.QCM[k][i][1:])
-            else :
-                print(k, " : ", self.QCM[k][i])
-        
-        r = int(input("Entrez le N° de votre réponse (1, 2, ou 3) : "))
-
-        return r
-        
-
-    def score_reponse(self, rep, i):
-        ''' calcul le score de la question n°i en fonction de la réponse
-        --> renvoie 0 ou 1'''
-        
-        assert type(self.QCM) != 'list', "QCM doit être un tableau."
-        assert rep > 0 and rep <= 3, "rep est compris entre 1 et 3."
-        assert i >= 0 and i <= 2, "i est compris entre 0 et 2."
-
-        rep_correcte = self.affiche_correction(i)[0]
-    
-        if rep_correcte == rep:
+        if self.rep_correcte == rep:
+            self.score += 1
             return 1
         else:
             return 0
@@ -134,3 +79,8 @@ class questionnairetressimple:
         pass
 
         print
+
+r = questionnairetressimple(DictioQCM)
+print(r.affichageQCM("drapeaux/Australie.png"))
+print(r.affiche_correction("drapeaux/Australie.png"))
+print(r.affiche_gag("drapeaux/Australie.png"))
