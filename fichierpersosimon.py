680411c85bb83286cd5947624affc3f17505572e
LOISON Simon Jacques Loic <simon.loison@0440274j.paysdelaloire.education>
2025-10-07 21:26:15 +0200

    J'ai fini de coder les fonctions pythons que j'ai implémenté dans fonctionclass.py Aurevoir (: !

--- fichierpersosimon.py
+++ fichierpersosimon.py
@@ -4,6 +4,7 @@ class questionnairetressimple:
 
     def __init__(self, QCM): 
         self.QCM = QCM
+        self.score = 0
 
     def affiche_correction(self, question):
         """ fonction qui affiche la correction à la question
@@ -13,21 +14,8 @@ class questionnairetressimple:
             if cle == question:
                 for i in range(len(valeur)):
                     if valeur[i][-1] == "*":
-                        self.reponse = valeur[i][:-1]  # on enlève l’étoile
-                        return (i+1, self.reponse)
-
-        return None
-
-    def affiche_gag(self, question):
-        """ fonction qui affiche la réponse drôle
-        --> renvoie un tuple avec (indice de la réponse , réponse spéciale drôle)"""
-        
-        for cle, valeur in self.QCM.items():
-            if cle == question:
-                for i in range(len(valeur)):
-                    if valeur[i][-1] == "!":
-                        self.reponse = valeur[i][:-1]  # on enlève le point d'!
-                        return (i+1, self.reponse)
+                        v = valeur[i][:-1]  # on enlève l’étoile
+                        return (i+1, v)
 
         return None
 
@@ -50,19 +38,21 @@ class questionnairetressimple:
         self.r = int(input("Entrez le N° de votre réponse (1, 2, ou 3) : "))
 
         return self.r
-        
 
     def score_reponse(self, rep, question):
         ''' calcul le score de la question n°i en fonction de la réponse
         --> renvoie 0 ou 1'''
-        
+
+        val = self.QCM[question]
+        rep_utilisateur = val[rep - 1][:-1]
+
         for cle, valeur in self.QCM.items():
             if cle == question:
                 for i in range(len(valeur)):
                     if valeur[i][-1] == "*":
-                        self.rep_correcte = valeur[i][:-1]
+                        rep_correcte = valeur[i][:-1]
     
-        if self.rep_correcte == rep:
+        if rep_correcte == rep_utilisateur:
             self.score += 1
             return 1
         else:
@@ -70,17 +60,17 @@ class questionnairetressimple:
 
     def passageQCM(self):
         ''' fonction de passage de QCM. Pour chaque question :
-        --> affiche la question puis les 3 propositions (fonction affichageQCM)
+        --> affiche la question puis les propositions (fonction affichageQCM)
         --> récupère le numéro de la réponse du candidat dans un tableau
         --> affiche la correction  (fonction affiche_correction)
         --> comptabilise le score (fonction score_reponse)'''
         
-        assert type(self.QCM) == list, "QCM doit être un tableau."
-        pass
-
-        print
+        for cle, val in self.QCM.items():
+            self.affichageQCM(cle)
+            self.score_reponse(self.r, cle)
+            print(self.score)
+        
+        print(f"Votre score est de : {self.score} !")
 
 r = questionnairetressimple(DictioQCM)
-print(r.affichageQCM("drapeaux/Australie.png"))
-print(r.affiche_correction("drapeaux/Australie.png"))
-print(r.affiche_gag("drapeaux/Australie.png"))
+r.passageQCM()
--- fonctionsclass.py
+++ fonctionsclass.py
@@ -1,4 +1,76 @@
-class QCM:
+from dictionnaires import DictioQCM
 
-    def __init__(self):
-        ""
+class questionnairetressimple:
+
+    def __init__(self, QCM): 
+        self.QCM = QCM
+        self.score = 0
+
+#########################################################################################################
+    """def affiche_correction(self, question):
+        ''' fonction qui affiche la correction à la question
+        --> renvoie un tuple avec (indice de la réponse , réponse correcte)'''
+        
+        for cle, valeur in self.QCM.items():
+            if cle == question:
+                for i in range(len(valeur)):
+                    if valeur[i][-1] == "*":
+                        v = valeur[i][:-1]  # on enlève l’étoile
+                        return (i+1, v)
+
+        return None
+
+    def affichageQCM(self, question):
+        ''' permet d'afficher la question du QCM contenue dans le tableau QCM.
+        les réponses sont ensuite proposées (l'étoile indiquant la bonne réponse est supprimée lors de l'affichage)'''
+        
+        print(f"Question : {question}")
+
+        for cle, valeur in self.QCM.items():
+            if cle == question:
+                for i in range(len(valeur)):
+                    if valeur[i][-1] == "*":
+                        print(i+1, " : ", valeur[i][:-1])
+                    elif valeur[i][-1] == "!":
+                        print(i+1, " : ", valeur[i][:-1])
+                    else : 
+                        print(i+1, " : ", valeur[i])
+
+        self.r = int(input("Entrez le N° de votre réponse (1, 2, ou 3) : "))
+
+        return self.r
+
+    def passageQCM(self):
+        ''' fonction de passage de QCM. Pour chaque question :
+        --> affiche la question puis les propositions (fonction affichageQCM)
+        --> récupère le numéro de la réponse du candidat dans un tableau
+        --> affiche la correction  (fonction affiche_correction)
+        --> comptabilise le score (fonction score_reponse)'''
+        
+        for cle, val in self.QCM.items():
+            self.affichageQCM(cle)
+            self.score_reponse(self.r, cle)
+            print(self.score)
+        
+        print(f"Votre score est de : {self.score} !")"""
+#########################################################################################################
+    
+    def score_reponse(self, rep, question):
+        ''' calcul le score de la question n°i en fonction de la réponse
+        --> renvoie 0 ou 1'''
+
+        val = self.QCM[question]
+        rep_utilisateur = val[rep - 1][:-1]
+
+        for cle, valeur in self.QCM.items():
+            if cle == question:
+                for i in range(len(valeur)):
+                    if valeur[i][-1] == "*":
+                        rep_correcte = valeur[i][:-1]
+    
+        if rep_correcte == rep_utilisateur:
+            self.score += 1
+            return 1
+        else:
+            return 0
+
