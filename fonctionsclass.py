89b8665166e06f9e6129f91a4823178697d7b3b2
Houmous106 <georgeshibou357@gmail.com>
2025-10-07 23:38:40 +0200

    Fusion des fonction class avec le tkinter + amélioration de l'interface avec Pierre et Armand, reste a voir pourquoi le fenetre.destroy ne marche pas ainsi que les boutons radios puisque lma fenetre ne s'ouvre pas quand je lance le programme

--- fonctionsclass.py
+++ fonctionsclass.py
@@ -1,4 +1,25 @@
 class QCM:
 
     def __init__(self):
-        ""
+        "doc"
+
+
+
+
+
+
+def liste_rep(dico=DictioQCM, question):
+        assert question <= 10, "Attention, il n'y a que 10 question"
+        i = 1
+        for liste_reponses in dico.values():
+            if i == question:
+                r = liste_reponses
+            i += 1
+        return r
+
+def liste_quest(dico=DictioQCM):
+        i = 1
+        q = []
+        for quest in dico.keys():
+            q.append(quest)
+        return q
