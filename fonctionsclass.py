9456561149d727bec0c68c0abbcc4d810727f3a6
Simon <Simonloison2@gmail.com>
2025-10-08 11:30:46 +0200

    tout marche bien c'est bien

--- fonctionsclass.py
+++ fonctionsclass.py
@@ -74,3 +74,30 @@ class questionnairetressimple:
         else:
             return 0
 
+
+
+
+class QCM:
+
+    def __init__(self):
+        "doc"
+
+
+
+
+
+
+    def liste_rep(question, dico=self.QCM):
+        assert question <= 10, "Attention, il n'y a que 10 question"
+        i = 1
+        for liste_reponses in dico.values():
+            if i == question:
+                r = liste_reponses
+            i += 1
+        return r
+
+    def liste_quest(dico=self.QCM):
+        i = 1
+        q = []
+        for quest in dico.keys():
+            q.append(quest)
